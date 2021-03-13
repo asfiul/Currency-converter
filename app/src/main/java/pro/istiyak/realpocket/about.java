11c9f5b1f3a602373b06cae97c3c22f56a932cef
Asfiul <asfiulislam80@gmail.com>
2021-03-13 17:49:14 +0600

    RealPocket

---
+++ app/src/main/java/pro/istiyak/realpocket/about.java
@@ -0,0 +1,14 @@
+package pro.istiyak.realpocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        this.setTitle("About");
+    }
+}
